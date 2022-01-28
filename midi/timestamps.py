edc55f9911ead61875a22effa780788f231d4f34
Manza12 <tritery@hotmail.com>
2022-01-28 14:22:51 +0100

    Improve timestamps plotting

--- midi/timestamps.py
+++ midi/timestamps.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pretty_midi as pm
-from plot import plot_timestamps
+from plot import plot_timestamps, plot_timestamps_bis
 
-midi = pm.PrettyMIDI('mozart_2.mid')
+midi = pm.PrettyMIDI('mozart_1.mid')
 
 timestamps = np.zeros(0)
 notes = np.zeros(0)
@@ -11,9 +11,11 @@ for note in midi.instruments[0].notes:
     timestamps = np.concatenate((timestamps, np.array([note.start])))
     notes = np.concatenate((notes, np.array([note.pitch])))
 
-# print(timestamps)
+indexes = np.argsort(timestamps)
+timestamps = timestamps[indexes]
+notes = notes[indexes]
 
-plot_timestamps(timestamps - np.min(timestamps), notes=notes)
+plot_timestamps_bis(timestamps, notes=notes)
 
 if __name__ == '__main__':
     pass

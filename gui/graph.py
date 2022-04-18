ee174d70e1d359e742f1669f64000b4989e5c44f
Manza12 <tritery@hotmail.com>
2022-04-18 16:21:26 +0200

    Start the GUI

---
+++ gui/graph.py
@@ -0,0 +1,201 @@
+import networkx as nx
+from acds import compute_acds
+from plot import plot_timestamps_radio
+from utils import log_distance, linear_clipped_weight
+import numpy as np
+
+
+def check_compatibility(graph, p_idx, c_idx):
+    if p_idx == 0:
+        return True
+
+    durations_p = graph.nodes[p_idx]['durations']
+    durations_c = graph.nodes[c_idx]['durations']
+
+    return np.all(durations_p[1:] == durations_c[0:len(durations_p)-1])
+
+
+def check_compatibility_polyphonic(graph, p_idx, c_idx):
+    if p_idx == 0:
+        return True
+
+    indexes_p = graph.nodes[p_idx]['indexes']
+    indexes_c = graph.nodes[c_idx]['indexes']
+
+    shift = 0
+    for idx in indexes_p[0]:
+        if idx not in indexes_c[0]:
+            shift += 1
+        else:
+            break
+
+    durations_p = graph.nodes[p_idx]['durations']
+    durations_c = graph.nodes[c_idx]['durations']
+
+    return np.all(durations_p[shift:] == durations_c[0:len(durations_p)-shift])
+
+
+def compute_weight(graph, p_idx, c_idx, error_weight=1., tempo_var_weight=1.):
+    if p_idx == 0:
+        return 0.
+
+    acd_p = graph.nodes[p_idx]['acd']
+    acd_c = graph.nodes[c_idx]['acd']
+    tempo_variation = log_distance(acd_p, acd_c)
+    error = graph.nodes[c_idx]['error']
+    weight_error = linear_clipped_weight(error, graph.graph['threshold'])
+
+    total_weight = error_weight * weight_error + tempo_var_weight * tempo_variation
+    return total_weight
+
+
+def create_graph(timestamps, frame_size=3, start_node=False, final_node=False, **kwargs):
+    # Initialisation
+    graph = nx.DiGraph(**kwargs)
+    K = np.zeros(0, dtype=np.int)
+    previous_nodes = []
+
+    # Start node
+    if start_node:
+        graph.add_node(0, acd=0., durations=np.zeros(0, dtype=np.int), pos=(-1, 0))
+        previous_nodes.append(0)
+
+    # Loop
+    for n in range(len(timestamps) - frame_size + 1):
+        # Initialize
+        current_nodes = []
+
+        # Select stretch
+        stretch = timestamps[n:n + frame_size]
+
+        # Compute aCD's
+        acds, acds_errors, acds_multiples, acds_durations = compute_acds(stretch, **kwargs)
+
+        # Length
+        K = np.concatenate((K, np.array([len(acds)])))
+
+        # Add nodes
+        for k in range(len(acds)):
+            graph.add_node((n, k),
+                           acd=acds[k],
+                           error=acds_errors[k],
+                           multiples=acds_multiples[k, :],
+                           durations=acds_durations[k, :],
+                           pos=(n, - (k - (K[-1] - 1) / 2)))
+            current_nodes.append((n, k))
+
+            for i in range(len(previous_nodes)):
+                compatible = check_compatibility(graph, previous_nodes[i], (n, k))
+
+                if compatible:
+                    weight = compute_weight(graph, previous_nodes[i], (n, k),
+                                            kwargs['error_weight'],
+                                            kwargs['tempo_var_weight'])
+                    graph.add_edge(previous_nodes[i], (n, k), weight=weight, color=(0., 0., 0.))
+
+        previous_nodes = current_nodes
+
+    # Final node
+    if final_node:
+        graph.add_node(1, acd=0., durations=np.zeros(0, dtype=np.int),
+                       pos=(graph.nodes[previous_nodes[0]]['pos'][0] + 1, 0))
+        for i in range(len(previous_nodes)):
+            graph.add_edge(previous_nodes[i], 1, weight=0., color=(0., 0., 0.))
+
+    return graph
+
+
+def create_polyphonic_graph(timestamps, notes, frame_size=2., hop_size=1., start_node=False, final_node=False,
+                            plot_steps=False, **kwargs):
+    # Initialisation
+    graph = nx.DiGraph(**kwargs)
+    K = np.zeros(0, dtype=np.int)
+    previous_nodes = []
+    frames = []
+
+    # Start node
+    if start_node:
+        graph.add_node(0, acd=0., durations=np.zeros(0, dtype=np.int), pos=(-1, 0))
+        previous_nodes.append(0)
+
+    # Loop
+    finish = False
+    t_0 = timestamps[0]
+    n = 0
+    while not finish:
+        # Initialize
+        current_nodes = []
+
+        # Select stretch
+        indexes = np.where(np.logical_and(t_0 <= timestamps, timestamps < t_0 + frame_size))
+        stretch = timestamps[indexes]
+        frames.append(stretch)
+
+        # Compute aCD's
+        acds, acds_errors, acds_multiples, acds_durations = compute_acds(stretch, **kwargs)
+
+        if plot_steps:
+            plot_timestamps_radio(stretch, acds, notes=notes[indexes], fig_size=(7., 4.))
+        # import matplotlib.pyplot as plt
+        # plt.figure()
+        # plt.scatter(timestamps[indexes], notes[indexes])
+        # plt.scatter(acds[0] * acds_multiples[0, :] + timestamps[indexes].min(), notes[indexes])
+        # plt.show()
+
+        # Length
+        K = np.concatenate((K, np.array([len(acds)])))
+
+        # Add nodes
+        for k in range(len(acds)):
+            graph.add_node((n, k),
+                           acd=acds[k],
+                           error=acds_errors[k],
+                           multiples=acds_multiples[k, :],
+                           durations=acds_durations[k, :],
+                           indexes=indexes,
+                           pos=(n, - (k - (K[-1] - 1) / 2)))
+            current_nodes.append((n, k))
+
+            for i in range(len(previous_nodes)):
+                compatible = check_compatibility_polyphonic(graph, previous_nodes[i], (n, k))
+
+                if compatible:
+                    weight = compute_weight(graph, previous_nodes[i], (n, k),
+                                            kwargs['error_weight'],
+                                            kwargs['tempo_var_weight'])
+                    graph.add_edge(previous_nodes[i], (n, k), weight=weight, color=(0., 0., 0.))
+
+        previous_nodes = current_nodes
+        n += 1
+        t_0 = t_0 + hop_size
+        if t_0 > timestamps.max():
+            finish = True
+
+    # Final node
+    if final_node:
+        graph.add_node(1, acd=0., durations=np.zeros(0, dtype=np.int),
+                       pos=(graph.nodes[previous_nodes[0]]['pos'][0] + 1, 0))
+        for i in range(len(previous_nodes)):
+            graph.add_edge(previous_nodes[i], 1, weight=0., color=(0., 0., 0.))
+
+    graph.graph['frames'] = frames
+    return graph
+
+
+def path_to_durations(graph, path, frame_size=3):
+    if not frame_size == 3:
+        raise NotImplementedError('Frame size should be 3')
+
+    path_durations = np.zeros(0, dtype=np.int)
+    for i, idx in enumerate(path):
+        node = graph.nodes[idx]
+        durations = node['durations']
+
+        if i == 0:
+            pass
+        if i == 1:
+            path_durations = np.concatenate((path_durations, durations))
+        else:
+            path_durations = np.concatenate((path_durations, durations[1:]))
+
+    return path_durations
---
+++ gui/main.py
@@ -0,0 +1,286 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+from matplotlib.figure import Figure
+from matplotlib.widgets import Slider
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import matplotlib.lines as lines
+
+import networkx as nx
+import numpy as np
+
+from graph import create_graph
+
+
+class ACDFrame(ttk.Frame):
+    layout_parameters = {'height': 500, 'width': 300,
+                         'left': 0, 'top': 0, 'rigth': 0, 'bottom': 0,
+                         'borderwidth': 0, 'relief': 'raised',
+                         'pad_x': 10, 'pad_y': 5}
+    acd_parameters = {}
+
+    def __init__(self, app, threshold=0.05, min_candidate=0.15, max_candidate=1., resolution=0.001, start_candidate=0.5,
+                 use_offsets=False, frame_length=2., overlap=2, **layout_parameters):
+        super().__init__(master=app.parameters_frame)
+
+        # ACD parameters
+        self.threshold = threshold
+        self.min_candidate = min_candidate
+        self.max_candidate = max_candidate
+        self.resolution = resolution
+        self.start_candidate = start_candidate
+
+        # Window parameters
+        self.use_offsets = use_offsets
+        self.frame_length = frame_length
+        self.overlap = overlap
+
+        # Layout
+        self.height = layout_parameters['height']
+        self.width = layout_parameters['width']
+        self.padding = (layout_parameters['left'], layout_parameters['top'],
+                        layout_parameters['rigth'], layout_parameters['bottom'])
+        self.borderwidth = layout_parameters['borderwidth']
+        self.relief = layout_parameters['relief']
+        self.pad_x = layout_parameters['pad_x']
+        self.pad_y = layout_parameters['pad_y']
+
+        self['height'] = self.height
+        self['width'] = self.width
+        self['padding'] = self.padding
+        self['borderwidth'] = self.borderwidth
+        self['relief'] = self.relief
+
+        self.grid(row=0, column=0, padx=self.pad_x, pady=self.pad_y)
+
+
+class PianoRollFrame(ttk.Frame):
+    def __init__(self, app, height=250, width=450, left=0, top=0, right=0, bottom=0, borderwidth=5, relief='raised',
+                 pad_x=10, pad_y=5, toolbar_on=False):
+        super().__init__(master=app.plot_frame, height=height, width=width)
+
+        # Size
+        self.height = height
+        self.width = width
+
+        # Parameters
+        self['padding'] = (left, top, right, bottom)
+        self['borderwidth'] = borderwidth
+        self['relief'] = relief
+
+        # Layout
+        self.grid(row=0, column=0, padx=pad_x, pady=pad_y)
+
+        # Figure
+        fig = Figure(figsize=(self.width / 100, self.height / 100), dpi=100)
+        t = np.arange(0, 3, .01)
+        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, anchor=tk.NW)
+
+        if toolbar_on:
+            toolbar = NavigationToolbar2Tk(canvas, self)
+            toolbar.update()
+            canvas.get_tk_widget().pack(side=tk.TOP)
+
+
+class FullGraphFrame(ttk.Frame):
+    def __init__(self, app, height=250, width=450, left=0, top=0, right=0, bottom=0, borderwidth=5, relief='raised',
+                 pad_x=10, pad_y=5):
+        super().__init__(master=app.plot_frame, height=height, width=width)
+
+        # Size
+        self.height = height
+        self.width = width
+
+        self['padding'] = (left, top, right, bottom)
+        self['borderwidth'] = borderwidth
+        self['relief'] = relief
+
+        self.grid(row=1, column=0, padx=pad_x, pady=pad_y)
+
+
+class GridFrame(ttk.Frame):
+    def __init__(self, app, height=250, width=450, left=0, top=0, right=0, bottom=0, borderwidth=0, relief='raised',
+                 pad_x=10, pad_y=5):
+        super().__init__(master=app.plot_frame, height=height, width=width)
+
+        # Size
+        self.height = height
+        self.width = width
+
+        self['padding'] = (left, top, right, bottom)
+        self['borderwidth'] = borderwidth
+        self['relief'] = relief
+
+        self.grid(row=0, column=1, padx=pad_x, pady=pad_y)
+
+        # Figure
+        timestamps = np.array([0., 0.98, 1.52])
+        self.plot_grid(timestamps, app.acd_frame)
+
+    def plot_grid(self, timestamps, acd_frame: ACDFrame):
+        threshold = acd_frame.threshold
+        min_cand = acd_frame.min_candidate
+        max_cand = acd_frame.max_candidate
+        res_cand = acd_frame.resolution
+        start_cand = acd_frame.start_candidate
+
+        n = len(timestamps)
+        notes = np.zeros(n)
+
+        fig = Figure(figsize=(self.width / 100, self.height / 100), dpi=100)
+
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, expand=True, fill=tk.BOTH, anchor=tk.NW)
+
+        ax = fig.gca()
+        points = ax.scatter(timestamps, notes)
+
+        fig.subplots_adjust(bottom=0.35)
+
+        ax.set_xlabel('Time (s)')
+        ax.get_yaxis().set_ticks([])
+        if not len(notes) == 0:
+            ax.set_ylim([np.min(notes) - 2, np.max(notes) + 2])
+        else:
+            ax.set_ylim([-0.1, 0.5])
+
+        line = ()
+        for i in range(n):
+            line = lines.Line2D([timestamps[i] - threshold, timestamps[i] + threshold], [notes[i], notes[i]], color='r')
+            ax.add_line(line)
+
+        def update(acd, idx):
+            x_lim = ax.get_xlim()
+            new_ticks = np.concatenate((np.arange(timestamps[idx], timestamps.min() - acd, -acd),
+                                        np.arange(timestamps[idx], timestamps.max() + acd, acd)))
+            ax.set_xticks(new_ticks, minor=False)
+            ax.set_xlim(x_lim)
+            ax.xaxis.grid(True, which='major')
+
+            grid_slider.valtext.set_text(str(round(acd, 3)) + ' s')
+            # note_slider.valtext.set_text(str(round(timestamps[idx], 3)) + ' s')
+
+        ax.legend([points, line], ['timestamps', 'threshold'])
+
+        ax_slider = fig.add_axes([0.15, 0.05, 0.65, 0.05])
+        grid_slider = Slider(
+            ax=ax_slider,
+            label='aCD',
+            valmin=min_cand,
+            valmax=max_cand,
+            valinit=start_cand,
+            valstep=res_cand
+        )
+
+        # ax_slider_note = fig.add_axes([0.15, 0.1, 0.65, 0.03])
+        # note_slider = Slider(
+        #     ax=ax_slider_note,
+        #     label='timestamp',
+        #     valmin=0,
+        #     valmax=n - 1,
+        #     valinit=0,
+        #     valstep=1
+        # )
+        grid_slider.on_changed(lambda val: update(val, 0))
+        # note_slider.on_changed(lambda val: update(grid_slider.val, val))
+
+        update(start_cand, 0)
+
+        # return fig
+
+
+class FocusedGraphFrame(ttk.Frame):
+    frame_size = 3
+    color = 0.9
+
+    def __init__(self, app, height=250, width=450, left=0, top=0, right=0, bottom=0, borderwidth=5, relief='raised',
+                 pad_x=10, pad_y=5):
+        super().__init__(master=app.plot_frame, height=height, width=width)
+
+        # Size
+        self.height = height
+        self.width = width
+
+        self['padding'] = (left, top, right, bottom)
+        self['borderwidth'] = borderwidth
+        self['relief'] = relief
+
+        self.grid(row=1, column=1, padx=pad_x, pady=pad_y)
+
+        # Figure
+        timestamps = np.array([0., 0.98, 1.52, 2.0, 2.53, 3.03])
+        self.plot_graph(timestamps, app.acd_frame)
+
+    def plot_graph(self, timestamps, acd_frame, current_frame=1):
+        threshold = acd_frame.threshold
+        min_cand = acd_frame.min_candidate
+        max_cand = acd_frame.max_candidate
+        res_cand = acd_frame.resolution
+
+        graph = create_graph(timestamps, threshold=threshold, min_cand=min_cand, max_cand=max_cand, res_cand=res_cand,
+                             frame_size=self.frame_size, error_weight=1., tempo_var_weight=1.)
+
+        pos = nx.get_node_attributes(graph, 'pos')
+        node_labels = {idx: round(graph.nodes[idx]['acd'], 3) for idx in graph.nodes}
+
+        color_circles = self.color * np.ones((len(graph), 3))
+
+        fig = Figure(figsize=(self.width / 100, self.height / 100), dpi=100)
+
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, expand=True, fill=tk.BOTH, anchor=tk.NW)
+
+        ax = fig.gca()
+        ax.axis('off')
+        ax.set_xlim([current_frame - 1 - 0.2, current_frame + 1 + 0.2])
+        nx.draw_networkx_nodes(graph, pos=pos, ax=ax, node_size=800, node_color=color_circles, node_shape='o')
+        nx.draw_networkx_edges(graph, pos=pos, ax=ax, arrows=True)
+        nx.draw_networkx_labels(graph, pos=pos, ax=ax, labels=node_labels, font_size=12)
+        canvas.draw()
+
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tk.TOP)
+
+        # nx.draw_networkx(graph, pos=pos, arrows=True, with_labels=True, labels=node_labels,
+        #                  node_size=300 * self.frame_size, node_color=color_circles, font_size=12)
+
+
+class RhythmTranscriptionApp(tk.Tk):
+    geometry_parameters = {'width': 1300, 'height': 600, 'x_offset': 100, 'y_offset': 50}
+
+    def __init__(self, title='Rhythm Transcription'):
+        super().__init__()
+
+        # Window size and positioning
+        # geometry_string = str(width) + 'x' + str(height) + '+' + str(x_offset) + '+' + str(y_offset)
+        # self.geometry(geometry_string)
+
+        # Title
+        self.title(title)
+
+        # Parameters frame
+        self.parameters_frame = ttk.Frame(master=self)
+        self.parameters_frame.grid(row=0, column=1)
+
+        self.acd_frame = ACDFrame(app=self, **ACDFrame.layout_parameters)
+
+        # Plot frame
+        self.plot_frame = ttk.Frame(master=self)
+        self.plot_frame.grid(row=0, column=0)
+
+        self.piano_roll_frame = PianoRollFrame(self)
+        self.full_graph_frame = FullGraphFrame(self)
+        self.grid_frame = GridFrame(self)
+        self.focused_graph_frame = FocusedGraphFrame(self)
+
+
+if __name__ == "__main__":
+    rhythm_app = RhythmTranscriptionApp()
+    rhythm_app.mainloop()
